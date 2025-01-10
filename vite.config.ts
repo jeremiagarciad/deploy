15ffe7b40fd1f39a16657d8391f0446807951521
Jeremia Garcia <softwareengeneer421@gmail.com>
2025-01-10 14:10:42 -0400

    delete: lines

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,16 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   base: "https://github.com/jeremiagarciad/deploy/",
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@components": path.resolve(__dirname, "./src/components"),
-    }
-  }
-   
+    },
+  },
 });
+
